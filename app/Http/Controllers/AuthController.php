425af943fb9b4f1a146ae61ce6271afb0ebc60e3
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2023-12-15 18:15:30 +0800

    final

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -41,25 +41,30 @@ class AuthController extends Controller
 
      
     public function register(Request $request)
-    { // Validation
+{try {
 
-        try {
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password), // Hash the password
-            ]);
+    $request->validate([
+        'name' => 'required|string',
+        'email' => 'required|email|unique:users,email',
+        'password' => 'required|string|confirmed|min:6',
+    ]);
 
-            return response()->json($user, 201);
-            return response(['message' => 'User registered successfully', 'user' => $user], 201);
-        } catch (\Throwable $e) {
-            //throw $th;
-           
-            return response()->json([
-                'status' => false,
-                'message' => $e->getMessage()
-            ], 500); 
-            return response("Errors");
-        }
+    try {
+        $request['role'] = '1';
+        $this->model->create($request->all());
+        return response(['message' => "Successfully created"], 201); // 201 Created
+    } catch (\Exception $e) {
+        return response(['message' => $e->getMessage()], 500); // 500 Internal Server Error
     }
+    // Log success
+    \Log::info('Registration successful');
+
+    return response(['message' => "Successfully created"], 201);
+} catch (\Exception $e) {
+    // Log the exception
+    \Log::error($e);
+
+    return response(['message' => $e->getMessage()], 500);
+}
+}
 }
